6a89a1a044cd1a3446f5c02e2fd5f5d9ef682a86
Daniyar <yerkinov.daniyar@yandex.kz>
2023-10-27 22:08:39 +0600

    practicing objects and classes

--- lesson27.php
+++ lesson27.php
@@ -2,11 +2,24 @@
 class User 
 {
     var $name;
-    var $access = 'смертный';
+    var $access = 'простой смертный';
+    function __construct($name)
+    {
+        $this->name = $name;
+    }
+    function sayHello()
+    {
+        echo $this->name . ': Всем привет! Меня зовут, ' . $this->name . '! И я - ' . $this->access . '!<br>';
+    }
+    function letsCode(User $user)
+    {
+        echo $this->name . ': Привет, ' . $user->name . '! Я - ' . $this->name . ', пойдем кодить!<br>';
+    }
 }
 
-$akim = new User();
-$akim->name = 'Аким';
+$akim = new User('Аким');
+$gulnaz = new User('Гульназ');
 
-$gulnaz = new User();
-$gulnaz->name = 'Гульназ';
+$akim->sayHello();
+$gulnaz->sayHello();
+$akim->letsCode($gulnaz);
