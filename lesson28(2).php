4ea88adb67d9004ab9eefca27055e4c1a0ea456c
Daniyar <yerkinov.daniyar@yandex.kz>
2023-10-31 21:06:46 +0600

    working with objects

--- lesson28(2).php
+++ lesson28(2).php
@@ -12,10 +12,25 @@ class Hero
         $this->health = $health;
         $this->strength = $strength;
     }
+
+    function takeDamage($amount)
+    {   
+        var_dump($this);
+        $this->health -= $amount;
+    }
+    function attack($enemyHero)
+    {
+        var_dump($this);
+        var_dump($enemyHero);
+        echo '--------------------------------' . "\n";
+        $enemyHero->takeDamage($this->strength);
+    }
 }
 
 $hero1 = new Hero(rand(1000, 2500), rand(100, 170));
 $hero2 = new Hero(rand(1000, 2500), rand(100, 170));
 
+$hero1->attack($hero2);
+echo '--------------------------------' . "\n";
 var_dump($hero1);
 var_dump($hero2);
