cbc2eda0d3589747c4cc80b6f218fb1a2afc199e
Daniyar <yerkinov.daniyar@yandex.kz>
2023-11-04 23:25:48 +0600

    fixed bugs in FOOL GAME

--- lesson29t3.php
+++ lesson29t3.php
@@ -8,32 +8,53 @@ $imyaKarty = ['6', '7', '8', '9', '10', 'Valet', 'Dama', 'Korol', 'Tuz'];
 $masti = ['Chervi', 'Kresti', 'Piki', 'Bubni'];
 $kolodaKart = [];
 
+// Создаем колоду карт
 foreach ($masti as $mast) {
-    foreach ($imyaKart as $karta) {
+    foreach ($imyaKarty as $karta) {
         $card = $karta . ' ' . $mast;
         $kolodaKart[] = $card;
     }
 }
 
 shuffle($kolodaKart);
+
+// Выбираем случайную масть как козырь
 $kozir = $masti[array_rand($masti)];
 echo "Козырь: $kozir\n";
 
 $players = ['Player1', 'Player2', 'Player3', 'Player4'];
 $razdacha = [];
-for ($i=0; $i<count($players); $i++) {
+
+// Раздаем карты игрокам
+for ($i = 0; $i < count($players); $i++) {
     $razdacha[$players[$i]] = array_slice($kolodaKart, 0, 6);
 }
 
 $kozirCounts = [];
 
+// Подсчитываем количество козырей у каждого игрока
 foreach ($players as $player) {
     $kozirCounts[$player] = 0;
     foreach ($razdacha[$player] as $card) {
-        $imyaKarty = $card[0];
-        $masti = substr($card, 1);
-        }
+        // Получаем масть карты (последний символ)
+        $masti = substr($card, -5); // <<<< Изменил эту строку
+
+        // Проверяем, является ли масть козырем
         if ($masti === $kozir) {
-            $kozirCounts++;
+            $kozirCounts[$player]++;
         }
     }
+}
+
+$maxKozirPlayer = '';
+$maxKozirCount = 0;
+
+// Определяем игрока с наибольшим количеством козырей
+foreach ($players as $player) {
+    if ($kozirCounts[$player] > $maxKozirCount) {
+        $maxKozirCount = $kozirCounts[$player];
+        $maxKozirPlayer = $player;
+    }
+}
+
+echo "Игрок $maxKozirPlayer имеет наибольшее количество козырей: $maxKozirCount козырей.\n";
