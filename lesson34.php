85e6567127c83b5bcddfc9f87dcd7acbfa8075b6
Daniyar <yerkinov.daniyar@yandex.kz>
2023-11-15 15:16:58 +0600

    connecting DB MySQL

--- lesson34.php
+++ lesson34.php
@@ -32,6 +32,7 @@ function getRestsFromPage($page)
 
 function getMaxPage($page)
 {
+    return 2;
     $subject = file_get_contents('https://restoran.kz/restaurant?page=' . $page);
     $pattern = '/<a.+?href="\/restaurant\?page=([0-9]+)">[0-9]+<\/a>/u';
     $result = [];
@@ -44,7 +45,22 @@ function getMaxPage($page)
     }
 }
 
-echo getMaxPage(10);
+$max = getMaxPage(1);
+$rests = [];
+for ($i=1; $i<=$max; $i++) {
+    $rests = array_merge($rests, getRestsFromPage($i));
+}
 
+$host = '127.0.0.1';
+$db   = 'test';
+$user = 'root';
+$pass = '';
+$charset = 'utf8';
 
-//print_r(getRestsFromPage(10));
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
