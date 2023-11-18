7e716e3f7d74d64643bdec459833ecb84afb081b
Daniyar <yerkinov.daniyar@yandex.kz>
2023-11-18 22:04:09 +0600

    fixed few bugs with parsing

---
+++ functions.php
@@ -0,0 +1,55 @@
+<?php
+
+function getRestsFromPage($page)
+{
+    $subject = file_get_contents('https://restoran.kz/restaurant?page=' . $page);
+    $pattern = '/<div class="mb-5">/u';
+    $blocks = preg_split($pattern, $subject);
+    unset($blocks[0]);
+
+    $rests = [];
+    foreach ($blocks as $block) {
+        $pattern = '/<a class="link-inherit-color" href="(.+?)">(.+?)<\/a>/u';
+        $result = [];
+        preg_match_all($pattern, $block, $result);
+
+        $rest = [
+            'name' => $result[2][0],
+            'link' => $result[1][0],
+        ];
+        $pattern = '/<li class="d-flex mr-5 mb-3"><svg class="icon icon_md flex-none mr-3" aria-hidden="true"><use xlink:href="(.+?)"><\/use><\/svg>(.+?)<\/li>/u';
+        $result = [];
+        preg_match_all($pattern, $block, $result);
+
+        $paramsMap = [
+            '#icon-plate' => 'cuisine',
+            '#icon-kz-tenge-in-circle' => 'price',
+            '#icon-lightning-in-circle' => 'options',
+        ];
+
+        foreach ($paramsMap as $k => $v) {
+            $index = array_search($k, $result[1]);
+            if ($index !== false) {
+                $rest[$v] = $result[2][$index];
+            }
+        }
+        $rests[] = $rest;
+    }
+
+    return $rests;
+}
+
+function getMaxPage($page)
+{
+    return 2;
+    $subject = file_get_contents('https://restoran.kz/restaurant?page=' . $page);
+    $pattern = '/<a.+?href="\/restaurant\?page=([0-9]+)">[0-9]+<\/a>/u';
+    $result = [];
+    preg_match_all($pattern, $subject, $result);
+    $max = max($result[1]);
+    if ($max <= $page) {
+        return $page;
+    } else {
+        return getMaxPage($max);
+    }
+}
--- lesson34.php
+++ lesson34.php
@@ -1,49 +1,6 @@
 <?php
 
-function getRestsFromPage($page)
-{
-    $subject = file_get_contents('https://restoran.kz/restaurant?page=' . $page);
-    $pattern = '/<a class="link-inherit-color" href="(.+?)">(.+?)<\/a>/u';
-    $result = [];
-    
-    preg_match_all($pattern, $subject, $result);
-    
-    $rests = [];
-    
-    foreach ($result[2] as $i => $name) {
-        $rests[] = [
-            'name' => $name,
-            'link' => $result[1][$i],
-        ];
-    }
-    
-    $pattern = '/<li class="d-flex mr-5 mb-3"><svg class="icon icon_md flex-none mr-3" aria-hidden="true"><use xlink:href="(.+?)"><\/use><\/svg>(.+?)<\/li>/u';
-    $result = [];
-    preg_match_all($pattern, $subject, $result);
-    
-    foreach ($rests as $i => $rest) {
-        $rests[$i]['cuisine'] = $result[2][$i*3];
-        $rests[$i]['price'] = $result[2][$i*3+1];
-        $rests[$i]['options'] = $result[2][$i*3+2];
-    }
-    
-    return $rests;
-}
-
-function getMaxPage($page)
-{
-    return 2;
-    $subject = file_get_contents('https://restoran.kz/restaurant?page=' . $page);
-    $pattern = '/<a.+?href="\/restaurant\?page=([0-9]+)">[0-9]+<\/a>/u';
-    $result = [];
-    preg_match_all($pattern, $subject, $result);
-    $max = max($result[1]);
-    if ($max <= $page) {
-        return $page;
-    } else {
-        return getMaxPage($max);
-    }
-}
+include './functions.php';
 
 $max = getMaxPage(1);
 $rests = [];
@@ -93,9 +50,8 @@ foreach ($rests as $rest) {
     $stmt->execute([
             ':name' => $rest['name'],
             ':link' => $rest['link'],
-            ':cuisine' => isset($rest['cuisine']) ? $rest['cuisine'] : '',
-            ':price' => isset($rest['price']) ? $rest['price'] : '',
+            ':cuisine' => isset($rest['cuisine']) ?? '',
+            ':price' => isset($rest['price']) ?? '',
             ':options' => isset($rest['options']) ? $rest['options'] : '',
         ]);
 }
-
