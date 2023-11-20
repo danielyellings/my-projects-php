99c033575620fd6a7739113265f1c70adfa97cb2
Daniyar <yerkinov.daniyar@yandex.kz>
2023-11-20 18:10:56 +0600

    separeted and added DB connection to db.php

--- lesson34.php
+++ lesson34.php
@@ -1,29 +1,13 @@
 <?php
 
 include './functions.php';
+include './db.php';
 
 $max = getMaxPage(1);
 $rests = [];
 for ($i=1; $i<=$max; $i++) {
     $rests = array_merge($rests, getRestsFromPage($i));
 }
-
-$host = '127.0.0.1';
-$db   = 'db_rests';
-$user = 'root';
-$pass = '';
-$charset = 'utf8';
-
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$opt = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
-# I Connection to DB
-$pdo = new PDO($dsn, $user, $pass, $opt);
 
 $stmt = $pdo->prepare("TRUNCATE TABLE `db_rests`.`rests`");
 
