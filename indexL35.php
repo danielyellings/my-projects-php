134ef65882abf0413b078834535edf708e510af5
Daniyar <yerkinov.daniyar@yandex.kz>
2023-11-22 06:59:27 +0600

    added LEFT JOIN to db

--- indexL35.php
+++ indexL35.php
@@ -8,9 +8,13 @@ $pdo = getPDO();
 /** *-этот символ означает передачу всех параметров без перечисления */
 $stmt = $pdo->prepare("
     SELECT
-        *
+        `rests`.*,
+        `categories`.`label` AS `category`
     FROM
         `rests`
+    LEFT JOIN
+        `categories`
+        ON `rests`.`category` = `categories`.`id`
     WHERE
         (`price_min`+`price_max`)/2 > :min
         AND (`price_min`+`price_max`)/2 < :max
@@ -21,4 +25,10 @@ $stmt->execute([
     ':min' => 10000,
     ':max' => 20000
 ]);
-print_r($stmt->fetchAll());
+
+$rests = $stmt->fetchAll();
+$stmt = $pdo->prepare("SELECT * FROM `categories`");
+$stmt->execute();
+$categories = $stmt->fetchAll();
+
+print_r($rests);
