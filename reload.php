8c942cfc4c98f08b9e83ee1153e24fd695759337
Daniyar <yerkinov.daniyar@yandex.kz>
2023-11-27 20:48:55 +0600

    fixed some code

--- reload.php
+++ reload.php
@@ -53,14 +53,20 @@ $stmt = $pdo->prepare("
             :name
         )
 ");
-
+$cuisinesMap = [];
 foreach ($cuisines as $cuisine) {
     $stmt->execute([
         ':name' => $cuisine, 
     ]);
+    $cuisinesMap[$cuisine] = $pdo->lastInsertId();
 }
 
+print_r($cuisinesMap);
+
 $stmt = $pdo->prepare("TRUNCATE TABLE `rests`");
+$stmt->execute();
+
+$stmt = $pdo->prepare("TRUNCATE TABLE `rests_cuisines`");
 $stmt->execute();
 
 # II Preparation of request
@@ -82,6 +88,18 @@ $stmt = $pdo->prepare("
             :options
         )
 ");
+
+$stmtRC = $pdo->prepare("
+        INSERT INTO
+            `rests_cuisines` (
+                `id_rest`,
+                `id_cuisine`
+            ) VALUES (
+                :id_rest,
+                :id_cuisine
+            )
+");
+
 # III Execution of request
 foreach ($rests as $rest) {
     $stmt->execute([
@@ -92,4 +110,12 @@ foreach ($rests as $rest) {
             ':price_max' => $rest['price']['max'],
             ':options' => isset($rest['options']) ? $rest['options'] : '',
         ]);
+    $restId = $pdo->lastInsertId();
+
+    foreach ($rest['cuisine'] as $cuisine) {
+        $stmtRC->execute([
+            ':id_rest' => $restId,
+            ':id_cuisine' => $cuisinesMap[$cuisine],
+        ]);
+    }
 }
